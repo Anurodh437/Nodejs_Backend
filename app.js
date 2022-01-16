96ea8434f910b03e2a18642d9e0b3bdd4a6eef11
Anurodh437 <anurodhdubey437@gmail.com>
2022-01-16 20:00:29 +0530

    setup mvc controller and express routing for better understanding and reusability

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./modals/blog");
-const { result } = require("lodash");
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
@@ -113,56 +112,8 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-//blog route
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 }) //sorts the data fetched from the database in the decreasing order means latest at the top
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-
-app.post("/blogs",(req,res) => {
-  console.log(req.body);
-  const blog = new Blog(req.body);
-  blog.save()
-  .then((result) => {
-    res.redirect('/blogs');
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-})
-
-app.get('/blogs/:id',(req,res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-  .then(result =>{
-    res.render('details',{blog: result,title: 'Blog Details'});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
-
-app.delete('/blogs/:id',(req,res) =>{
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id).then(result =>{
-    res.json({redirect:'/blogs'})
-  })
-  .catch(err =>{
-    console.log(err);
-  })
-})
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
+//blog routes
+app.use('/blogs',blogRoutes);
 
 // 404 page
 app.use((req, res) => {
